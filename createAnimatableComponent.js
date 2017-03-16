7a848990bc8a51718e2a1a9befe19ffbc9fdbefb
br4nnigan <laz.brannigan@gmail.com>
2017-03-16 15:34:49 +0100

    use delay for transition

--- createAnimatableComponent.js
+++ createAnimatableComponent.js
@@ -80,10 +80,11 @@ function makeInterpolatedStyle(compiledAnimation, animationValue) {
   return wrapStyleTransforms(style);
 }
 
-function transitionToValue(transitionValue, toValue, duration, easing, useNativeDriver = false) {
+function transitionToValue(transitionValue, toValue, duration, easing, useNativeDriver = false, delay) {
   if (duration || easing) {
     Animated.timing(transitionValue, {
       toValue,
+      delay: delay,
       duration: duration || 1000,
       easing: typeof easing === 'function' ? easing : EASING_FUNCTIONS[easing || 'ease'],
       useNativeDriver,
@@ -138,6 +139,7 @@ export default function createAnimatableComponent(WrappedComponent) {
     };
 
     static defaultProps = {
+      delay: 0,
       iterationCount: 1,
       onAnimationBegin() {},
       onAnimationEnd() {},
@@ -251,11 +253,11 @@ export default function createAnimatableComponent(WrappedComponent) {
     }
 
     componentWillReceiveProps(props) {
-      const { animation, duration, easing, transition, onAnimationBegin, onAnimationEnd } = props;
+      const { animation, delay, duration, easing, transition, onAnimationBegin, onAnimationEnd } = props;
 
       if (transition) {
         const values = getStyleValues(transition, props.style);
-        this.transitionTo(values, duration, easing);
+        this.transitionTo(values, duration, easing, delay);
       } else if (animation !== this.props.animation) {
         if (animation) {
           if (this.delayTimer) {
@@ -376,7 +378,7 @@ export default function createAnimatableComponent(WrappedComponent) {
       });
     }
 
-    transitionTo(toValues, duration, easing) {
+    transitionTo(toValues, duration, easing, delay) {
       const { currentTransitionValues } = this.state;
       const toValuesFlat = flattenStyle(toValues);
       const transitions = {
@@ -390,7 +392,7 @@ export default function createAnimatableComponent(WrappedComponent) {
         const transitionStyle = this.state.transitionStyle[property];
         const transitionValue = this.state.transitionValues[property];
         if (!needsInterpolation && transitionStyle && transitionStyle === transitionValue) {
-          transitionToValue(transitionValue, toValue, duration, easing, this.props.useNativeDriver);
+          transitionToValue(transitionValue, toValue, duration, easing, false, delay);
         } else {
           let currentTransitionValue = currentTransitionValues[property];
           if (typeof currentTransitionValue === 'undefined' && this.props.style) {
@@ -411,7 +413,7 @@ export default function createAnimatableComponent(WrappedComponent) {
       Object.keys(toValues).forEach((property) => {
         const transitionValue = this.state.transitionValues[property];
         const toValue = toValues[property];
-        transitionToValue(transitionValue, toValue, duration, easing, this.props.useNativeDriver);
+        transitionToValue(transitionValue, toValue, duration, easing, false, delay);
       });
     }
 

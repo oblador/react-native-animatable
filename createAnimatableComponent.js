bac8b3f948871c0d9deae1d4ce370d405d9d68e3
alphasp <gmerudotcom@gmail.com>
2017-07-09 18:46:07 +0800

    migrate deprecated React.PropTypes

--- createAnimatableComponent.js
+++ createAnimatableComponent.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { Animated, Easing } from 'react-native';
 import wrapStyleTransforms from './wrapStyleTransforms';
 import getStyleValues from './getStyleValues';

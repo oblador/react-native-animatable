2544cc2caef0f7acdf48538a3928adcb7a2fd93b
Kyle Roach <kroach.work@gmail.com>
2017-07-13 14:11:18 -0400

    Replace React.PropTypes with 'prop-types'

--- createAnimatableComponent.js
+++ createAnimatableComponent.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Animated, Easing } from 'react-native';
 import wrapStyleTransforms from './wrapStyleTransforms';
 import getStyleValues from './getStyleValues';
